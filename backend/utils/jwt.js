2425b678a36c59ab737012c3b614dc0d0625d2b2
lizasilent <elizavetad@yandex.ru>
2021-03-30 16:35:32 +0300

    это что, каникулы?

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const NODE_ENV = 'production';
-const JWT_SECRET = 'dev-secret';
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const generateSign = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
