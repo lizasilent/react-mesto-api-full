51af476d2d731505ec94c02d34fb0bc0a81bb8dc
lizasilent <elizavetad@yandex.ru>
2021-04-03 23:06:29 +0300

    это что

--- frontend/src/utils/api2.js
+++ frontend/src/utils/api2.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://api.mesto.lizasilent.nomoredomains.icu';
  
 export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
